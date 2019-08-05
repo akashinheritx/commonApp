4a46c429cb535da6487a423ad4c32a96027acd7a
Yogesh Barot <yogi1616>
2019-08-05 14:47:41 +0530

    sendMessage file modified

--- services/sendMessage.service.js
+++ services/sendMessage.service.js
@@ -2,12 +2,11 @@ var http = require('http');
 var urlencode = require('urlencode');
 // var msg = urlencode('hello js');
 
-var username = 'info@businessecosys.com';
-// var username = 'info@commonapp.com';
-//var hash = '56788c26992f71dc207bf6cc7f9b5c8106ec12306955585fca77f133e2d74e97'; // The hash key could be found under Help->All Documentation->Your hash key. Alternatively you can use your Textlocal password in plain text.
-var hash = 'aa211d4bf4c347bffc46b045ebe6b69a19f9863107e484e7184ce0239486cdfc';
-var sender = 'DRCOWS';
-// var sender = 'COMMONAPP';
+var username = 'info@commonapp.com'; //username is required to register with textlocal
+
+var hash = 'abcdefghijklmnopqrstuvwxyz'; // you will get a hashcode from textlocal
+
+var sender = 'COMMONAPP'; //sender is required to register with textlocal
 
 // username=info@businessecosys.com&hash=f4da7aee3f3df520b531ea800f2813c186045acfcb6a7c953721dd051072461b&sender=DRCOWS&numbers=8154993956&message=Dear%20Mooer%2C%20Your%20DearCows%20message%20Dear%20Mooer%2C%20Your%20account%20has%20been%20inactivated.%20Call%20on%201800-300-283-07%20for%20more%20information.
 exports.sendMessage = function (toNumber, message) {
